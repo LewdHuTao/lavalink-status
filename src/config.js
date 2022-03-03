ce1b665e87de2661e013e528253b7f8e921b3d8c
Dotto Dotto <90232327+dottodottoo@users.noreply.github.com>
2022-03-03 20:15:47 +0800

    update: `lavalink events`, `prettier`

--- src/config.js
+++ src/config.js
@@ -1,34 +1,31 @@
 module.exports = {
-    token: "ODkzNjY5MjAzODMwMjU1Njc2.YVe0TQ.KC8Ebh44ZLx3QGWAfxUP1vqJ2nY",
-    channel: "932917612835401749",
+    token: "Bot Token",
+    channel: "ChannelId",
 
     nodes: [
         {
-            host: "fin1.vertexnodes.com",
-            password: "youshallnotpass",
-            port: 25554,
+            host: "",
+            password: "",
+            port: 443,
             retryDelay: 300000,
-            retryAmount: 5,
-            identifier: "Node-01",
-            secure: false
+            identifier: "",
+            secure: true
         },
         {
-            host: "dono-01.danbot.host",
-            password: "youshallnotpass",
-            port: 1689,
+            host: "",
+            password: "",
+            port: 443,
             retryDelay: 300000,
-            retryAmount: 5,
-            identifier: "Node-02",
-            secure: false
+            identifier: "",
+            secure: true
         },
         {
-            host: "dono-03.danbot.host",
-            password: "youshallnotpass",
-            port: 1382,
+            host: "",
+            password: "",
+            port: 443,
             retryDelay: 300000,
-            retryAmount: 5,
-            identifier: "Node-03",
-            secure: false
+            identifier: "",
+            secure: true
         }
     ]
 }
