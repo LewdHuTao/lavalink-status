f4cb8b56fcff4651c568f8555b3bb80f0e02505f
LewdHuTao <90232327+LewdHuTao@users.noreply.github.com>
2022-03-12 18:07:08 +0800

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -24,36 +24,15 @@ module.exports = async (client) => {
 
         let info = [];
         info.push(`${color} Node          :: ${node.options.identifier}`);
-        info.push(
-          `${color} Status        :: ${
-            node.connected ? "Connected [🟢]" : "Disconnected [🔴]"
-          }`
-        );
+        info.push(`${color} Status        :: ${node.connected ? "Connected [🟢]" : "Disconnected [🔴]"}`);
         info.push(`${color} Player        :: ${node.stats.players}`);
         info.push(`${color} Used Player   :: ${node.stats.playingPlayers}`);
-        info.push(
-          `${color} Uptime        :: ${moment
-            .duration(node.stats.uptime)
-            .format(" d [days], h [hours], m [minutes], s [seconds]")}`
-        );
+        info.push(`${color} Uptime        :: ${moment.duration(node.stats.uptime).format(" d [days], h [hours], m [minutes], s [seconds]")}`);
         info.push(`${color} Cores         :: ${node.stats.cpu.cores} Core(s)`);
-        info.push(
-          `${color} Memory Usage  :: ${prettyBytes(
-            node.stats.memory.used
-          )}/${prettyBytes(node.stats.memory.reservable)}`
-        );
-        info.push(
-          `${color} System Load   :: ${(
-            Math.round(node.stats.cpu.systemLoad * 100) / 100
-          ).toFixed(2)}%`
-        );
-        info.push(
-          `${color} Lavalink Load :: ${(
-            Math.round(node.stats.cpu.lavalinkLoad * 100) / 100
-          ).toFixed(2)}%`
-        );
-        all.push(info.join("\n"));
-      });
+        info.push(`${color} Memory Usage  :: ${prettyBytes(node.stats.memory.used)}/${prettyBytes(node.stats.memory.reservable)}`);
+        info.push(`${color} System Load   :: ${(Math.round(node.stats.cpu.systemLoad * 100) / 100).toFixed(2)}%`);
+        info.push(`${color} Lavalink Load :: ${(Math.round(node.stats.cpu.lavalinkLoad * 100) / 100).toFixed(2)}%`);
+        all.push(info.join("\n"));});
 
       const rembed = new Embed()
         .setColor(Util.resolveColor("#2F3136"))
