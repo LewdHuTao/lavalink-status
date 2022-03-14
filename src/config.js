97393ed3e230a259a3e2ab032ccaa5ef601353ae
Dotto Dotto <90232327+dottodottoo@users.noreply.github.com>
2022-03-14 14:18:08 +0800

    builder

--- src/config.js
+++ src/config.js
@@ -1,28 +1,28 @@
 module.exports = {
-    token: "ODkzNjY5MjAzODMwMjU1Njc2.YVe0TQ.Ry6ihEJEYcTNX_-AgcTZ6ihYxsw",
-    channel: "932917612835401749",
+    token: "Bot Token",
+    channel: "ChannelId",
 
     nodes: [
         {
-            host: "fin1.vertexnodes.com",
-            password: "youshallnotpass",
-            port: 25554,
+            host: "",
+            password: "",
+            port: 443,
             retryDelay: 300000,
             identifier: "Node-01",
             secure: false
         },
         {
-            host: "dono-01.danbot.host",
-            password: "youshallnotpass",
-            port: 1689,
+            host: "",
+            password: "",
+            port: 443,
             retryDelay: 300000,
             identifier: "Node-02",
             secure: false
         },
         {
-            host: "dono-03.danbot.host",
-            password: "youshallnotpass",
-            port: 1382,
+            host: "",
+            password: "",
+            port: 443,
             retryDelay: 300000,
             identifier: "Node-03",
             secure: false
