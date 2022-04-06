9b0d785af2b92bf2420a6b8b16fdee8951f3ea1e
Dotto Dotto <90232327+dottodottoo@users.noreply.github.com>
2022-04-06 10:58:08 +0800

    retryAmount

--- src/config.js
+++ src/config.js
@@ -1,31 +1,34 @@
 module.exports = {
-    token: "Bot Token",
-    channel: "ChannelId",
+  token: "Bot Token",
+  channel: "ChannelId",
 
-    nodes: [
-        {
-            host: "",
-            password: "",
-            port: 443,
-            retryDelay: 300000,
-            identifier: "",
-            secure: true
-        },
-        {
-            host: "",
-            password: "",
-            port: 443,
-            retryDelay: 300000,
-            identifier: "",
-            secure: true
-        },
-        {
-            host: "",
-            password: "",
-            port: 443,
-            retryDelay: 300000,
-            identifier: "",
-            secure: true
-        }
-    ]
-} 
+  nodes: [
+    {
+      host: "",
+      password: "",
+      port: 443,
+      retryDelay: 300000,
+      retryAmount: 15,
+      identifier: "",
+      secure: true,
+    },
+    {
+      host: "",
+      password: "",
+      port: 443,
+      retryDelay: 300000,
+      retryAmount: 15,
+      identifier: "",
+      secure: true,
+    },
+    {
+      host: "",
+      password: "",
+      port: 443,
+      retryDelay: 300000,
+      retryAmount: 15,
+      identifier: "",
+      secure: true,
+    },
+  ],
+};
