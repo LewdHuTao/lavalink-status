334618c2fc968c2a79f8f5117b3a6e6f9334627d
LewdHuTao <imanimtiyaz242@gmail.com>
2024-08-24 15:46:44 +0800

    feat: auto fetch domain from host

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ module.exports = {
   channelId: process.env.channelId || "", //Channel Id you want to send the message
 
   webMonitor: true, // Set to false if you don't want to use web-monitor
-  domain: "localhost", // Need to use for badge
   expressPort: process.env.expressPort || 3000, // Port for web monitor
 
   nodes: [
